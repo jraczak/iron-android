0501ba3aac85d0bfb9835299053aa43bce57471c
Justin <jraczak@gmail.com>
2016-09-27 18:26:11 -0700

    Populate basic exercise class

--- app/src/main/java/com/justinraczak/android/squadgoals/models/Exercise.java
+++ app/src/main/java/com/justinraczak/android/squadgoals/models/Exercise.java
@@ -1,7 +1,44 @@
 package com.justinraczak.android.squadgoals.models;
 
+import java.util.HashMap;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.Required;
+
 /**
  * Created by Justin on 9/27/16.
  */
-public class Exercise {
+public class Exercise extends RealmObject {
+
+
+    private String id;
+    @Required @PrimaryKey
+    private String name;
+    @Required
+    private HashMap<String, String[]> muscles;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public HashMap<String, String[]> getMuscles() {
+        return muscles;
+    }
+
+    public void setMuscles(HashMap<String, String[]> muscles) {
+        this.muscles = muscles;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
