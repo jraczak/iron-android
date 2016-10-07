425d8bbb9b47be9f6d4f20f9b524ee4b21158a6e
Justin <jraczak@gmail.com>
2016-10-07 17:42:56 +0200

    Add id to exercise button. Set onClickListener in log workout fragment

--- app/src/main/java/com/justinraczak/android/squadgoals/LogWorkoutActivityFragment.java
+++ app/src/main/java/com/justinraczak/android/squadgoals/LogWorkoutActivityFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +19,17 @@ public class LogWorkoutActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_log_workout, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_log_workout, container, false);
+        Button addExerciseButton = (Button) view.findViewById(R.id.button_add_exercise);
+        addExerciseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Tried to add exercise", Toast.LENGTH_LONG).show();
+            }
+        });
+        return view;
+
     }
+
 }
