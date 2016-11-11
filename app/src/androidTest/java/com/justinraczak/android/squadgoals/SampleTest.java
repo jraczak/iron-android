2ad1b189ee9341bda1fd067de889df1d9d48608d
Justin <jraczak@gmail.com>
2016-11-10 17:39:02 -0800

    Add broken espresso test

---
+++ app/src/androidTest/java/com/justinraczak/android/squadgoals/SampleTest.java
@@ -0,0 +1,102 @@
+package com.justinraczak.android.squadgoals;
+
+
+import android.os.Build;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class SampleTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Test
+    public void sampleTest() {
+
+        allowPermissionsIfNeeded();
+
+        ViewInteraction textView = onView(
+                allOf(withText("Squad Goals"),
+                        childAtPosition(
+                                allOf(withId(R.id.action_bar),
+                                        childAtPosition(
+                                                withId(R.id.action_bar_container),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("Squad Goals")));
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.email_sign_in_button),
+                        childAtPosition(
+                                allOf(withId(R.id.email_login_form),
+                                        childAtPosition(
+                                                withId(R.id.login_form),
+                                                0)),
+                                2),
+                        isDisplayed()));
+        button.check(matches(isDisplayed()));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+
+    private void allowPermissionsIfNeeded()  {
+        //  mDevice = UiDevice.getInstance(getInstrumentation());
+        if (Build.VERSION.SDK_INT >= 23) {
+            UiObject allowPermissions = UiDevice.getInstance(getInstrumentation()).findObject(new UiSelector().text("Allow"));
+            if (allowPermissions.exists()) {
+                try {
+                    Log.d("acceptPermissionScript", "Trying to click accept dialog");
+                    allowPermissions.click();
+                } catch (UiObjectNotFoundException e) {
+                    Log.e(e.toString(), "There is no permissions dialog to interact with ");
+                }
+            }
+        }
+    }
+}
