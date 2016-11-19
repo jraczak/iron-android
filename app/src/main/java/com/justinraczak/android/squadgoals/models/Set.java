21b492142492449e12f9c0363036cbd55639d796
Justin <jraczak@gmail.com>
2016-11-18 18:39:37 -0800

    Modify set and workout models

--- app/src/main/java/com/justinraczak/android/squadgoals/models/Set.java
+++ app/src/main/java/com/justinraczak/android/squadgoals/models/Set.java
@@ -3,6 +3,8 @@ package com.justinraczak.android.squadgoals.models;
 import java.util.Date;
 
 import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.Required;
 
 /**
  * Created by Justin on 11/18/16.
@@ -10,5 +12,10 @@ import io.realm.RealmObject;
 
 public class Set extends RealmObject{
 
+    @PrimaryKey
+    private int id;
+    @Required
     private Date date;
+    @Required
+    private Workout workout;
 }
--- app/src/main/java/com/justinraczak/android/squadgoals/models/Workout.java
+++ app/src/main/java/com/justinraczak/android/squadgoals/models/Workout.java
@@ -1,6 +1,7 @@
 package com.justinraczak.android.squadgoals.models;
 
 import java.util.Date;
+import java.util.UUID;
 
 import io.realm.RealmList;
 import io.realm.RealmObject;
@@ -14,10 +15,52 @@ import io.realm.annotations.Required;
 public class Workout extends RealmObject {
 
     @PrimaryKey
-    private int id;
+    private String id;
     @Required
     private String name;
     public RealmList<Set> sets;
     private Date date;
 
+    public Workout(String name, RealmList<Set> sets, Date date) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.date = date;
+        this.sets = sets;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public RealmList<Set> getSets() {
+        return sets;
+    }
+
+    public void setSets(RealmList<Set> sets) {
+        this.sets = sets;
+    }
+
+    public void addSet(Set set) {
+        this.sets.add(set);
+    }
 }
